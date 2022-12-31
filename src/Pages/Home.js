d662b4f929dbde2fe1dc554f51db18f695adc0fc
KooTaehyeon <vcro12123@naver.com>
2022-12-31 16:08:07 +0900

    ✨	Feat 공공데이터 api 가공

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,14 +4,7 @@ import Map from '../Components/Map';
 import useFetch from '../hook/useApi';
 const Home = () => {
   const { apiData, setApiData } = useContext(schoolApi);
-  const [listData, setListData] = useState([
-    {
-      id: 0,
-      name: '',
-      x: 0,
-      y: 0,
-    },
-  ]);
+  const [listData, setListData] = useState([]);
 
   const [value] = useFetch(
     `http://apis.data.go.kr/1532000/KCG_Station_Position/list_view?serviceKey=${process.env.REACT_APP_API_URL}&rowsCount=5&startPage=1`
@@ -20,7 +13,16 @@ const Home = () => {
   useEffect(() => {}, []);
   console.log(apiData, '클릭이벤트 실행시');
   const onClickHanddler = () => {
-    setApiData(value.children);
+    const newValue = value.children.map((item, idx) => {
+      const body = {
+        id: idx,
+        name: item.children[0].value,
+        x: item.children[1].value,
+        y: item.children[2].value,
+      };
+      return body;
+    });
+    setApiData(newValue);
   };
   return <div onClick={onClickHanddler}>검색창 </div>;
 };
